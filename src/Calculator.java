2593ac37a16ff9249f55cfba8b5407d642038aea
Jakub <jakubkrys@gmail.com>
2021-01-16 22:57:16 +0100

    #7 Dodawanie liczb i wyciąganie średniej za pomocą metody

--- src/Calculator.java
+++ src/Calculator.java
@@ -7,4 +7,16 @@ public class Calculator {
     int sum5Arguments (int a, int b, int c, int d, int e){
         return a+b+c+d+e;
     }
+
+    int sumArray(int[] values){
+        int sum = 0;
+        for (int v : values){
+            sum  += v;
+        }
+        return sum;
+    }
+
+    double avg (int[] values){
+        return (double)sumArray(values)/values.length;
+    }
 }
