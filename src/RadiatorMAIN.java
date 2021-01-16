c42c41fd70829a5cd04e78062e8dfb5b65bd4154
Jakub <jakubkrys@gmail.com>
2021-01-16 20:43:32 +0100

    #1 Wyświetlenie informacji o kaloryferach za pomocą metody

---
+++ src/RadiatorMAIN.java
@@ -0,0 +1,38 @@
+public class RadiatorMAIN {
+    public static void main(String[] args) {
+
+        char[] red = {'r','e','d'};
+        char[] blue = {'b','l','u','e'};
+        char[] green = {'g','r','e','e','n'};
+        Radiator r1 = new Radiator (red, 55, 1200);
+        Radiator r2 = new Radiator (blue, 120, 5150);
+        Radiator r3 = new Radiator (green, 15, 500);
+
+        Radiator[] radiators = new Radiator[3];
+        radiators[0] = r1;
+        radiators[1] = r2;
+        radiators[2] = r3;
+
+        // Wyświetlenie danych za pomocą pętli for
+        for(Radiator r : radiators){
+            System.out.print("Color: ");
+            for (char c : r.color){
+                System.out.print(c);
+            }
+            System.out.println();
+            System.out.println("Weight: "+r.weight);
+            System.out.println("Power: "+r.power+"W");
+            System.out.println();
+        }
+
+        // Wyświetlenie danych za pomocą metody display
+        System.out.println("----------------------------");
+        System.out.println();
+        for (Radiator r : radiators){
+            r.display();
+        }
+
+        radiators[1].weight = 119;
+        radiators[1].display();
+    }
+}
